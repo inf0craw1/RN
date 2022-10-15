8a56a7a4cfc588fe58cbf6d04e68a3df5d9057be
inf0craw1 <inf0craw1@naver.com>
2022-10-15 18:45:14 +0900

    feat: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,12 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       rules: {
         quotes: ['error', 'single', {allowTemplateLiterals: true}],
+        indent: ['error', 4],
         tabWidth: 4,
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
         'no-return-assign': ['error', 'always'],
-        indent: ['error', 4],
         'object-curly-spacing': ['warn', 'never'],
         'prettier/prettier': [
           'error',
