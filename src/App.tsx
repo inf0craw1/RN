c63fe954307c7d335016d7c3852d2f0d608c386b
inf0craw1 <inf0craw1@naver.com>
2022-10-15 18:36:03 +0900

    feat: move App.tsx to src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+import CustomButton from './components/CustomButton';
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <CustomButton></CustomButton>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default App;
