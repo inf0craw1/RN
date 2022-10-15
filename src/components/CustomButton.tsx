16deee112e96d9488bcfd7b1f7516c36e9608693
inf0craw1 <inf0craw1@naver.com>
2022-10-15 18:36:28 +0900

    feat: CustomButton

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Pressable, Text} from 'react-native';
+
+const CustomButton = () => {
+    return (
+        <Pressable>
+            <Text>Button</Text>
+        </Pressable>
+    );
+};
+
+export default CustomButton;
