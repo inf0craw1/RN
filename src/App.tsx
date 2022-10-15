056f5bf1b88f458e435636dc2b349d74e00b86c8
inf0craw1 <inf0craw1@naver.com>
2022-10-15 18:57:41 +0900

    feat: style change

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import CustomButton from './components/CustomButton';
 
 const App = () => {
 
     const handlePress = () => {
         console.log('press');
-    }
+    };
 
     return (
         <SafeAreaView>
-            <View>
+            <View style={styles.view}>
+                <Text>Test</Text>
                 <CustomButton text='custom button' onPress={handlePress}></CustomButton>
             </View>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    view: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default App;
