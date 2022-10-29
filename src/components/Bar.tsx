21253feff6437e03103aa32d89a7e9453e4d4495
inf0craw1 <inf0craw1@naver.com>
2022-10-30 00:29:46 +0900

    feat: add Bar component

---
+++ src/components/Bar.tsx
@@ -0,0 +1,16 @@
+const Bar = () => {
+    return (
+        <View style={styles.bar}>
+            
+        </View>
+    )
+};
+
+const styels = StyleSheet.create({
+    bar: {
+        height: 12,
+        width: ''
+    }
+})
+
+export default Bar;
