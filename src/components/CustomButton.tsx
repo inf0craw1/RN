b236e69589bde21a645601e772c26c488e54645e
inf0craw1 <inf0craw1@naver.com>
2022-10-30 00:29:53 +0900

    feat: add styles

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, Text} from 'react-native';
+import {Pressable, StyleSheet, Text} from 'react-native';
 
 interface ButtonProps {
     text: string;
@@ -8,10 +8,24 @@ interface ButtonProps {
 
 const CustomButton = ({text, onPress}: ButtonProps) => {
     return (
-        <Pressable onPress={onPress} accessibilityLabel={text}>
-            <Text>{text || 'button'}</Text>
+        <Pressable style={styles.styledPressable} onPress={onPress} accessibilityLabel={text}>
+            <Text style={styles.styledText}>{text || 'button'}</Text>
         </Pressable>
     );
 };
+
+const styles = StyleSheet.create({
+    styledPressable: {
+        backgroundColor: '#424ef3',
+        padding: 12,
+        borderRadius: 9,
+    },
+    styledText: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+    
+})
 
 export default CustomButton;
