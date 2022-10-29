5478e4b9e0a8259a41359fb65080d08dfd14415d
inf0craw1 <inf0craw1@naver.com>
2022-10-30 00:30:40 +0900

    feat: add GameScreen in Stack.Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen';
+import GameScreen from './screens/GameScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Game" component={GameScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
