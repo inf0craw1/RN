4d89db43779e0a624ca2eb9775a6b9d3bb2078e9
inf0craw1 <inf0craw1@naver.com>
2022-11-03 00:31:39 +0900

    feat: add animatiofeat: add animation with animated view

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,18 +1,33 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Animated, StyleSheet, useWindowDimensions } from "react-native";
 
-const Bar = () => {
+type BarProps = {
+    delay: number;
+}
+
+const Bar = ({delay}: BarProps) => {
+    const { height } = useWindowDimensions();
+    const animatedBarPosition = useRef(new Animated.Value(-20)).current;
+
+    useEffect(() => {
+        Animated.timing(animatedBarPosition, { 
+            toValue: height + 20,
+            duration: 5000,
+            delay: delay,
+            useNativeDriver: true,
+        }).start();
+    }, []);
+
     return (
-        <View style={styles.bar}>
+        <Animated.View style={[styles.bar, { transform: [{translateY: animatedBarPosition }]}]}>
             
-        </View>
+        </Animated.View>
     )
 };
 
 const styles = StyleSheet.create({
     bar: {
         position: 'absolute',
-
         width: '100%',
         height: 16,
         backgroundColor: '#424ef3'
