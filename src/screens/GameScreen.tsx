95297a91f9384261691943b48ab7f66f4fc9ea6a
inf0craw1 <inf0craw1@naver.com>
2022-11-03 00:32:38 +0900

    feat: add delay data in barData array

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -10,11 +10,11 @@ type BarDataType = {
 
 const GameScreen = ({navigation}: {navigation: NavigationStackProp}) => {
     
-    const barData = [{line: 1, position: 300},{line: 0, position: 500},{line: 2, position: 700},{line: 1, position: 100}];
+    const barData = [{line: 1, delay: 3000},{line: 0, delay: 5000},{line: 2, delay: 7000},{line: 1, delay: 1000}];
 
     const renderBars = (line: number) => {
         const currentLineBars = barData.filter(e => e.line === line);
-        const res = currentLineBars.map((e, idx) => <Bar key={`Bar-${idx}`} />);
+        const res = currentLineBars.map((e, idx) => <Bar key={`Bar-${idx}`} delay={e.delay} />);
 
         return res;
     }
