8cd03d5ca0c4d22004b9f60b555f93c24a70c3cb
inf0craw1 <inf0craw1@naver.com>
2022-11-05 18:51:51 +0900

    feat: added randomBarDatas

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,52 +1,63 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NavigationStackProp} from 'react-navigation-stack';
-import Bar from "../components/Bar";
+import Bar from '../components/Bar';
 
 type BarDataType = {
-    line: number,
-    position: number
-}
+  line: number;
+  delay: number;
+};
 
 const GameScreen = ({navigation}: {navigation: NavigationStackProp}) => {
-    
-    const barData = [{line: 1, delay: 3000},{line: 0, delay: 5000},{line: 2, delay: 7000},{line: 1, delay: 1000}];
+  const getRandomBarData = (num = 100) => {
+    let randomBars: BarDataType[] = [];
+    let accDelays = 3000;
 
-    const renderBars = (line: number) => {
-        const currentLineBars = barData.filter(e => e.line === line);
-        const res = currentLineBars.map((e, idx) => <Bar key={`Bar-${idx}`} delay={e.delay} />);
-
-        return res;
+    for (let i = 0; i < num; i++) {
+      accDelays += Math.floor(Math.random() * 30) * 100;
+      randomBars.push({
+        line: Math.floor(Math.random() * 3) + 1,
+        delay: accDelays,
+      });
     }
+  };
 
-    return (
-        <SafeAreaView style={{flex: 1}}>
-            <View style={styles.lineWrapper}>
-                <View style={styles.line}>
-                    {renderBars(0)}
-                </View>
-                <View style={[styles.line, {borderColor: '#000', borderLeftWidth: 1, borderRightWidth: 1}]}>
+  const barData = getRandomBarData();
 
-                {renderBars(1)}
-                </View>
-                <View style={styles.line}>
+  const renderBars = (line: number) => {
+    const currentLineBars = barData.filter(e => e.line === line);
+    const res = currentLineBars.map((e, idx) => (
+      <Bar key={`Bar-${idx}`} delay={e.delay} />
+    ));
 
-                {renderBars(2)}
-                </View>
-            </View>
-        </SafeAreaView>
-    )
-}
+    return res;
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.lineWrapper}>
+        <View style={styles.line}>{renderBars(0)}</View>
+        <View
+          style={[
+            styles.line,
+            {borderColor: '#000', borderLeftWidth: 1, borderRightWidth: 1},
+          ]}>
+          {renderBars(1)}
+        </View>
+        <View style={styles.line}>{renderBars(2)}</View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    lineWrapper: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    line: {
-        flex: 1,
-    },
-    
-})
+  lineWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  line: {
+    flex: 1,
+  },
+});
 
 export default GameScreen;
