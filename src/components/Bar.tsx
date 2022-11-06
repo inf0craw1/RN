3f31afe2fb84766db99afd30456408bde871b232
inf0craw1 <inf0craw1@naver.com>
2022-11-06 23:57:38 +0900

    feat: bar height edited

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,38 +1,39 @@
-import React, { useEffect, useRef } from "react";
-import { Animated, StyleSheet, useWindowDimensions } from "react-native";
+import React, {useEffect, useRef} from 'react';
+import {Animated, StyleSheet, useWindowDimensions} from 'react-native';
 
 type BarProps = {
-    delay: number;
-}
+  delay: number;
+};
 
 const Bar = ({delay}: BarProps) => {
-    const { height } = useWindowDimensions();
-    const animatedBarPosition = useRef(new Animated.Value(-20)).current;
+  const {height} = useWindowDimensions();
+  const animatedBarPosition = useRef(new Animated.Value(-20)).current;
 
-    useEffect(() => {
-        Animated.timing(animatedBarPosition, { 
-            toValue: height + 20,
-            duration: 5000,
-            delay: delay,
-            useNativeDriver: true,
-        }).start();
-    }, []);
+  useEffect(() => {
+    Animated.timing(animatedBarPosition, {
+      toValue: height + 20,
+      duration: 5000,
+      delay: delay,
+      useNativeDriver: true,
+    }).start();
+  }, []);
 
-    return (
-        <Animated.View style={[styles.bar, { transform: [{translateY: animatedBarPosition }]}]}>
-            
-        </Animated.View>
-    )
+  return (
+    <Animated.View
+      style={[
+        styles.bar,
+        {transform: [{translateY: animatedBarPosition}]},
+      ]}></Animated.View>
+  );
 };
 
 const styles = StyleSheet.create({
-    bar: {
-        position: 'absolute',
-        width: '100%',
-        height: 16,
-        backgroundColor: '#424ef3'
-
-    }
-})
+  bar: {
+    position: 'absolute',
+    width: '100%',
+    height: 30,
+    backgroundColor: '#424ef3',
+  },
+});
 
 export default Bar;
